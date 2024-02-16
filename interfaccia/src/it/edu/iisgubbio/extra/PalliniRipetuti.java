5195ef3652e99c2b2afbb4b579556d4e7d459d0b
BigHouse71 <149068613+BigHouse71@users.noreply.github.com>
2024-02-16 10:15:55 +0100

    Caricamento completo
    adesso i pallini si allargano e si in grandiscono

--- interfaccia/src/it/edu/iisgubbio/extra/PalliniRipetuti.java
+++ interfaccia/src/it/edu/iisgubbio/extra/PalliniRipetuti.java
@@ -12,12 +12,16 @@ import javafx.scene.control.Button;
 import javafx.scene.Scene;
 
 public class PalliniRipetuti extends Application{
+
+	public static final int NUMERO_PALLINI = 10;
+	double distanza=200,grandezza=7;
+	boolean allontanati=true;
 	Pane quadro=new Pane();
 	double angolo=0;
 	Circle pallino=new Circle(7);
-	double x[]=new double[6];
-	Circle pallini[]=new Circle[6];
-	double y[]=new double[6];
+	double x[]=new double[NUMERO_PALLINI];
+	Circle pallini[]=new Circle[NUMERO_PALLINI];
+	double y[]=new double[NUMERO_PALLINI];
 	Timeline timer = new Timeline(new KeyFrame(Duration.millis(10), x -> timer()));
 	public void start(Stage finestra) {
 		pallino.setLayoutX(200);
@@ -26,7 +30,7 @@ public class PalliniRipetuti extends Application{
 			pallini[i]=new Circle(7);
 			quadro.getChildren().add(pallini[i]);
 		}
-		
+
 		quadro.getChildren().add(pallino);
 		timer.setCycleCount(Animation.INDEFINITE);
 		timer.play();
@@ -34,25 +38,52 @@ public class PalliniRipetuti extends Application{
 		finestra.setScene(scena);
 		finestra.show();
 	}
-	
+
 	public void timer() {
-		
-		for(int i=0,bho = 0;i<x.length;i++,bho+=60) {
-			x[i]=(200+100*Math.cos(((angolo+bho)*Math.PI)/180));
-			
+		int gradi=360/pallini.length;
+		for(int i=0,angolazione =0;i<x.length;i++,angolazione+=gradi) {
+			x[i]=(200+distanza*Math.cos(((angolo+angolazione)*Math.PI)/180));
+
 		}
-		for(int i=0,bho = 0;i<y.length;i++,bho+=60) {
-			y[i]=(200+100*Math.sin(((bho+angolo)*Math.PI)/180));
+		for(int i=0,angolazione = 0;i<y.length;i++,angolazione+=gradi) {
+			y[i]=(200+distanza*Math.sin(((angolazione+angolo)*Math.PI)/180));
 		}
-		
+
 		for(int i=0;i<pallini.length;i++) {
 			pallini[i].setLayoutX(x[i]);
 			pallini[i].setLayoutY(y[i]);
 		}
-		
-		
+
+
 		angolo++;
-		
+
+		if(distanza==0) {
+			allontanati=true;
+		}
+		if(distanza==201) {
+			allontanati=false;
+		}
+		if(allontanati) {
+			distanza++;
+			if(grandezza>=20) {
+
+			}else {
+				grandezza+=0.04;
+				for(int i=0;i<pallini.length;i++) {
+					pallini[i].setRadius(grandezza);
+				}
+			}
+
+		}else {
+			distanza--;
+			if(grandezza>=7) {
+				grandezza-=0.09;
+				for(int i=0;i<pallini.length;i++) {
+					pallini[i].setRadius(grandezza);
+				}
+			}
+
+		}
 	}
 	public static void main(String[] args) {
 		launch(args);
